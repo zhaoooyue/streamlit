39f5d2cb865d952f1d0b3398f2d511a7c0bac2e7
Zhao Yue <yue.zhao@oaknorth.ai>
2020-04-10 00:45:49 +0800

    some draft

--- dva.py
+++ dva.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("CSE6242 Data Visualization Project")
 
@@ -9,3 +11,18 @@ This website serves as the purpose of demonstrating `CSE6242 Data Visualization 
 1. Use the sidebar to select an algorithm
 2. Interact and have fun!
 """)
+
+option = st.sidebar.selectbox(
+    'Which algorithm do you want to try?',
+     ["Decision Tree", "Random Forests", "Clustering"])
+
+st.markdown(f"""
+### Analyzing `{option}` algorithm
+""")
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.info("Accuracy vs Iterations")
+st.line_chart(chart_data)
