f81ed46245681477e72d942d95d9c38a441deba8
Zhao Yue <yue.zhao@oaknorth.ai>
2020-04-10 17:48:40 +0800

    add some stuff

--- dva.py
+++ dva.py
@@ -4,26 +4,28 @@ import numpy as np
 
 st.title("CSE6242 Data Visualization Project")
 
-st.markdown("""
+st.markdown(
+    """
 This website serves as the purpose of demonstrating `CSE6242 Data Visualization Project` for 2020 Spring
 
 ## How to use this website?
 1. Use the sidebar to select an algorithm
 2. Interact and have fun!
-""")
+"""
+)
 
 option = st.sidebar.selectbox(
-    'Which algorithm do you want to try?',
-     ["Decision Tree", "Random Forests", "Clustering"])
+    "Which algorithm do you want to try?",
+    ["Decision Tree", "Random Forests", "Clustering"],
+)
 
-st.markdown(f"""
+st.markdown(
+    f"""
 ### Analyzing `{option}` algorithm
-""")
+"""
+)
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.info("Accuracy vs Iterations")
 st.line_chart(chart_data)
-
